d4a231d2f8dd32b040b1ac62541676e635d71fe6
Viktor Sandström <sandstrom.viktor@gmail.com>
2024-12-01 17:26:23 +0100

    day1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
   let mut v1 = vec!();
   let mut v2 = vec!();
 
-  for x in fs::read_to_string("../input").expect("file parsing").lines() {
+  for x in fs::read_to_string("day1/input").expect("file parsing").lines() {
     let mut row = x.trim().split("   ");
     let s1 = row.next().unwrap().parse::<i32>().unwrap();
     let s2 = row.last().unwrap().parse::<i32>().unwrap();
@@ -16,17 +16,18 @@ fn main() {
 
   v1.sort();
   v2.sort();
-  let mut sum= 0;
-
-  for (x, y) in v1.iter().zip(v2.iter()) {
-    sum += i32::abs(x - y);
-  }
+  let sum = v1 .iter() .zip(v2.iter()) .fold(0, |acc, (x, y)| { acc + i32::abs(x-y) });
 
   let mut sum2 = 0;
   for e in v1 {
-    for x in &v2 {
+    let mut found = false;
+    'inner: for x in &v2 {
       if *x == e {
+        found = true;
         sum2 += *x;
+      } else if found && *x != e {
+        // break early
+        break 'inner;
       }
     }
   }
