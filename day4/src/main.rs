a5e796fc70a241a883156cc81b1926bd43446156
Viktor Sandström <Viktor Sandstrm>
2024-12-04 09:52:16 +0100

    d4

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -7,8 +7,7 @@ fn main() {
   for row in &data {
     for win in row.windows(4) {
       match win {
-        ['X','M','A','S'] => c+=1,
-        ['S','A','M','X'] => c+=1,
+        ['X','M','A','S'] | ['S','A','M','X'] => c+=1,
         _ => ()
       }
     }
@@ -18,31 +17,18 @@ fn main() {
   for win in data.windows(4) {
     for i in 0..l {
       if i < l-3 {
-        match &[
-          win[0][i],
-          win[1][i+1],
-          win[2][i+2],
-          win[3][i+3]
-        ] {
-          ['X','M','A','S'] => c+=1,
-          ['S','A','M','X'] => c+=1,
+        match &[win[0][i], win[1][i+1], win[2][i+2], win[3][i+3]] {
+          ['X','M','A','S'] | ['S','A','M','X'] => c+=1,
           _ => ()
         }
         
-        match &[
-          win[0][i+3],
-          win[1][i+2],
-          win[2][i+1],
-          win[3][i]
-        ] {
-          ['X','M','A','S'] => c+=1,
-          ['S','A','M','X'] => c+=1,
+        match &[win[0][i+3], win[1][i+2], win[2][i+1], win[3][i]] {
+          ['X','M','A','S']| ['S','A','M','X'] => c+=1,
           _ => ()
         }
       }
       match &[win[0][i],win[1][i],win[2][i],win[3][i]] {
-        ['X','M','A','S'] => c+=1,
-        ['S','A','M','X'] => c+=1,
+        ['X','M','A','S'] | ['S','A','M','X'] => c+=1,
         _ => ()
       }
     }
@@ -57,18 +43,8 @@ fn main() {
   for win in data.windows(3) {
     for i in 0..l {
       if i < l-2 {
-        match &[
-          win[0][i],
-          win[0][i+2],
-          win[1][i+1],
-          win[2][i],
-          win[2][i+2],
-
-        ] {
-          ['M','M','A','S','S'] => c+=1,
-          ['S','M','A','S','M'] => c+=1,
-          ['S','S','A','M','M'] => c+=1,
-          ['M','S','A','M','S'] => c+=1,
+        match &[win[0][i], win[0][i+2], win[1][i+1], win[2][i], win[2][i+2]] {
+          ['M','M','A','S','S'] | ['S','M','A','S','M'] | ['S','S','A','M','M'] | ['M','S','A','M','S'] => c+=1,
           _ => ()
         } 
       }
