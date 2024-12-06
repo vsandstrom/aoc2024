ab60bfc12abf153de47010935874b2ae1c062c43
Viktor Sandström <Viktor Sandstrm>
2024-12-06 12:15:44 +0100

    d6

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,9 +1,10 @@
-use std::fs;
+use std::{fs, usize};
 use std::collections::{HashMap, HashSet, hash_map::Entry};
+use std::cmp::Ordering;
 
 
 fn main() {
-  let file = fs::read_to_string("day5/input").expect("file parsing");
+  let file = fs::read_to_string("day5/test").expect("file parsing");
   let mut data = file.split("\n\n");
   let mut bck_rules: HashMap<i32, HashSet<i32>> = HashMap::new();
   let mut fwd_rules: HashMap<i32, HashSet<i32>> = HashMap::new();
@@ -38,28 +39,29 @@ fn main() {
     }
   }
 
+  let mut  filtered_orders = vec!();
 
   let mut sum = 0;
   'outer: for row in data.next().unwrap().lines() {
     let page_order = row.split(",").map(|s| s.parse::<i32>().unwrap()).collect::<Vec<i32>>();
     for (i, page) in page_order.iter().enumerate() {
-      if let Some(rule) = fwd_rules.get_key_value(page) {
-        let rule = rule.1;
+      if let Some(rule) = fwd_rules.get(page) {
         let order = &page_order[(i+1)..];
         if !order.is_empty() {
           for p in order {
             if !rule.contains(p) {
+              filtered_orders.push(page_order);
               continue 'outer;
             }
           }
         }
       }
-      if let Some(rule) = bck_rules.get_key_value(page) {
-        let rule = rule.1;
+      if let Some(rule) = bck_rules.get(page) {
         let order = &page_order[..i];
         if !order.is_empty() {
           for p in order {
             if !rule.contains(p) {
+              filtered_orders.push(page_order);
               continue 'outer;
             }
           }
@@ -71,4 +73,30 @@ fn main() {
   }
   println!("{}", sum);
 
+  for mut page_order in filtered_orders { 
+    // for every page
+    // loop over the rest of the pages, and check if all is in order.
+    // if not in order, remove the faulty one, shift every number to the right, including the
+    // current page. 
+    // Start over from the top.
+   
+    // let mut i = 0;
+    // while i < page_order.len() {
+    //   let page = page_order[i];
+    //   let slc = page_order[i..].to_vec();
+    //   if let Some(rule) = fwd_rules.get(&page) {
+    //     for (j, p) in slc.iter().enumerate() {
+    //       if !rule.contains(p) {
+    //         let mut k = i;
+    //         while k <= j {
+    //           page_order.swap(k, i+1);
+    //           println!("{:?}", page_order);
+    //           k+=1;
+    //         }
+    //         i+=1;
+    //       }
+    //     }
+    //   }
+    // }
+  }
 }
---
+++ day6/src/main.rs
@@ -0,0 +1,115 @@
+use std::{collections::HashSet, fs};
+
+#[derive(Clone, Copy, Default, Debug, Hash, PartialEq, Eq)]
+enum Dir {
+  #[default]
+  Up,
+  Down,
+  Right,
+  Left
+}
+
+#[derive(Eq, Hash, PartialEq, Debug, Clone, Copy)]
+struct Guard {
+  pub pos: (i32, i32),
+  pub dir: Dir
+}
+
+impl Guard {
+  fn traverse(&self) -> (i32, i32) {
+    match self.dir {
+      Dir::Up =>    (self.pos.0 - 1, self.pos.1),
+      Dir::Down =>  (self.pos.0 + 1, self.pos.1),
+      Dir::Right => (self.pos.0, self.pos.1 + 1),
+      Dir::Left =>  (self.pos.0, self.pos.1 - 1)
+    }
+  }
+
+  fn traverse_with_pos(&self) -> Self {
+    match self.dir {
+      Dir::Up =>    Self{pos:(self.pos.0 - 1, self.pos.1), dir: self.dir},
+      Dir::Down =>  Self{pos:(self.pos.0 + 1, self.pos.1), dir: self.dir},
+      Dir::Right => Self{pos:(self.pos.0, self.pos.1 + 1), dir: self.dir},
+      Dir::Left =>  Self{pos:(self.pos.0, self.pos.1 - 1), dir: self.dir}
+    }
+  }
+
+  fn rotate(&mut self) {
+    match self.dir {
+      Dir::Up => self.dir = Dir::Right,
+      Dir::Down => self.dir = Dir::Left,
+      Dir::Right => self.dir = Dir::Down,
+      Dir::Left => self.dir = Dir::Up
+    }
+  }
+}
+
+fn main() {
+  let mut pos: HashSet<(i32, i32)> = HashSet::new();
+  let mut startposition = Guard{pos: (0,0), dir: Dir::Up};
+  let mut walk: HashSet<(i32, i32)> = HashSet::new();
+  // let height = 10;
+  // let width = 10;
+  let height = 130;
+  let width = 130;
+  for (i, l) in fs::read_to_string("day6/input").expect("no input").lines().enumerate() {
+    for (j,c) in l.chars().enumerate() {
+      if c == '#' {
+        pos.insert((i.try_into().expect("unconvertable"), j.try_into().expect("unconvertable")));
+      }
+      if c == '^' {
+        startposition.pos = (i.try_into().expect("unconvertable"), j.try_into().expect("unconvertable"));
+      }
+    }
+  };
+
+  let mut guard = startposition;
+  while guard.pos.0 >= 0 && guard.pos.0 < height && guard.pos.1 >= 0 && guard.pos.1 < width {
+    walk.insert(guard.pos);
+    // potential position
+    let p = guard.traverse();
+    if pos.contains(&p) {
+    // if obsticle, rotate
+      guard.rotate();
+    } else {
+    // else move to potential position, update walked tiles
+      guard.pos = p;
+    }
+  }
+  println!("{}", walk.len());
+
+
+  let mut opts = 0;
+  // choose position to be blocked. 
+  for i in 0..height {
+    for j in 0..width {
+      // new run:
+      let mut guard = startposition;
+      let mut walk_with_pos: HashSet<Guard> = HashSet::new();
+      // while inside room
+      while guard.pos.0 >= 0 && guard.pos.0 < height && guard.pos.1 >= 0 && guard.pos.1 < width {
+        // if we are treading in our own steps
+        if walk_with_pos.contains(&guard) {
+          opts+=1;
+          break;
+        } 
+
+        walk_with_pos.insert(guard);
+
+        // potential position
+        let p = guard.traverse_with_pos();
+        if pos.contains(&p.pos) || p.pos == (i,j) {
+        // if obsticle or placed object, rotate
+          guard.rotate();
+        } else {
+        // else move to potential position, update walked tiles
+          guard = p;
+
+        }
+      }
+
+    }
+  }
+
+  println!("{}", opts);
+}
